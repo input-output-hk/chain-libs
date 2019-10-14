44c863eeaf271de2c70b6f7f3fffe9ba7344f105
dkijania <dariusz.kijania@iohk.io>
2019-10-14 15:37:36 +0200

    some gaps in apply_transaction_tests

--- chain-impl-mockchain/src/ledger/tests/transaction_tests.rs
+++ chain-impl-mockchain/src/ledger/tests/transaction_tests.rs
@@ -2,8 +2,9 @@
 
 use crate::fee::FeeAlgorithm;
 use crate::{
+    accounting::account::LedgerError::NonExistent,
     fragment::Fragment,
-    ledger::{check::TxVerifyError, Entry, Error::{TransactionMalformed,UtxoError}, Ledger},
+    ledger::{check::TxVerifyError, Entry, Error::{TransactionMalformed,UtxoError, Account, AccountInvalidSignature}, Ledger},
     testing::{
         arbitrary::{
             AccountStatesVerifier, ArbitraryValidTransactionData, NonZeroValue, UtxoVerifier,
@@ -267,7 +268,6 @@ pub fn transaction_with_nonexisting_utxo_input() {
     let faucet = AddressData::utxo(Discrimination::Test);
     let receiver = AddressData::utxo(Discrimination::Test);
     let value = Value(100);
-    let fake_utxo = TestGen::hash();
     let message = requests::create_initial_transaction(Output::from_address(
         faucet.address.clone(),
         value,
@@ -300,22 +300,128 @@ pub fn transaction_with_nonexisting_utxo_input() {
 
 #[test]
 pub fn transaction_nonexisting_account_input() {
+    let faucet = AddressData::utxo(Discrimination::Test);
+    let fake_account = AddressData::account(Discrimination::Test);
+    let receiver = AddressData::utxo(Discrimination::Test);
+    let value = Value(100);
 
+    let message = requests::create_initial_transaction(Output::from_address(
+        faucet.address.clone(),
+        value,
+    ));
+
+    let (block0_hash, ledger) =
+        ledger::create_initial_fake_ledger(&[message], ConfigBuilder::new().build()).unwrap();
+    
+    let signed_tx = TransactionBuilder::new()
+        .with_input(fake_account.make_input(value, None))
+        .with_output(receiver.make_output(value))
+        .authenticate()
+        .with_witness(&block0_hash, &fake_account)
+        .seal();
+
+    let fragment_id = Fragment::Transaction(signed_tx.clone()).hash();
+    let fees = ledger.get_ledger_parameters();
+    assert_err!(
+        Account { source: NonExistent },
+        ledger.apply_transaction(&fragment_id, &signed_tx, &fees)
+    );
 }
-
 
 #[test]
 pub fn duplicated_account_transaction() {
+    let faucet = AddressData::account(Discrimination::Test);
+    let receiver = AddressData::account(Discrimination::Test);
+    let message = requests::create_initial_transaction(Output::from_address(
+        faucet.address.clone(),
+        Value(200),
+    ));
+
+    let (block0_hash, ledger) =
+        ledger::create_initial_fake_ledger(&[message], ConfigBuilder::new().build()).unwrap();
+    let signed_tx = TransactionBuilder::new()
+        .with_input(faucet.make_input(Value(100),None))
+        .with_output(receiver.make_output(Value(100)))
+        .authenticate()
+        .with_witness(&block0_hash, &faucet)
+        .seal();
+    let fragment_id = Fragment::Transaction(signed_tx.clone()).hash();
+    let fees = ledger.get_ledger_parameters();
+    let result = ledger.apply_transaction(&fragment_id, &signed_tx, &fees);
+    assert!(result.is_ok(),"first transaction should be successful");
+    let (ledger,_) = result.unwrap();
+
+    assert!(ledger.apply_transaction(&fragment_id, &signed_tx, &fees).is_err());
+}
+
+#[test]
+pub fn repeated_account_transaction() {
+    let mut faucet = AddressData::account(Discrimination::Test);
+    let receiver = AddressData::account(Discrimination::Test);
+    let message = requests::create_initial_transaction(Output::from_address(
+        faucet.address.clone(),
+        Value(200),
+    ));
+
+    let (block0_hash, ledger) =
+        ledger::create_initial_fake_ledger(&[message], ConfigBuilder::new().build()).unwrap();
+    let signed_tx = TransactionBuilder::new()
+        .with_input(faucet.make_input(Value(100),None))
+        .with_output(receiver.make_output(Value(100)))
+        .authenticate()
+        .with_witness(&block0_hash, &faucet)
+        .seal();
+    let fragment_id = Fragment::Transaction(signed_tx.clone()).hash();
+    let fees = ledger.get_ledger_parameters();
+    let result = ledger.apply_transaction(&fragment_id, &signed_tx, &fees);
+    assert!(result.is_ok(),"first transaction should be successful");
+
+    let (ledger,_) = result.unwrap();
+    faucet.confirm_transaction();
+
+    let signed_tx = TransactionBuilder::new()
+        .with_input(faucet.make_input(Value(100),None))
+        .with_output(receiver.make_output(Value(100)))
+        .authenticate()
+        .with_witness(&block0_hash, &faucet)
+        .seal();
+    let fragment_id = Fragment::Transaction(signed_tx.clone()).hash();
+    let fees = ledger.get_ledger_parameters();
+    assert!(ledger.apply_transaction(&fragment_id, &signed_tx, &fees).is_ok(),"second transaction should be successful");
 
 }
 
 #[test]
-pub fn account_spending_counter_max_transaction_with_all_funds() {
+pub fn transaction_with_incorrect_account_spending_counter() {
+    let faucet = AddressData::account(Discrimination::Test);
+    let receiver = AddressData::account_with_spending_counter(Discrimination::Test, 1);
+    let message = requests::create_initial_transaction(Output::from_address(
+        faucet.address.clone(),
+        Value(200),
+    ));
 
-}
+    let (block0_hash, ledger) =
+        ledger::create_initial_fake_ledger(&[message], ConfigBuilder::new().build()).unwrap();
 
+    let signed_tx = TransactionBuilder::new()
+        .with_input(faucet.make_input(Value(100),None))
+        .with_output(receiver.make_output(Value(100)))
+        .authenticate()
+        .with_witness(&block0_hash, &faucet)
+        .seal();
 
-#[test]
-pub fn account_spending_counter_max_transaction_with_partial_funds() {
+    let fragment_id = Fragment::Transaction(signed_tx.clone()).hash();
+    let fees = ledger.get_ledger_parameters();
+    let result = ledger.apply_transaction(&fragment_id, &signed_tx, &fees);
+    assert!(result.is_ok(),"first transaction should be successful");
+    let (ledger,_) = result.unwrap();
 
+    let signed_tx = TransactionBuilder::new()
+        .with_input(receiver.make_input(Value(100),None))
+        .with_output(faucet.make_output(Value(100)))
+        .authenticate()
+        .with_witness(&block0_hash, &receiver)
+        .seal();
+    let fragment_id = Fragment::Transaction(signed_tx.clone()).hash();
+    assert!(ledger.apply_transaction(&fragment_id, &signed_tx, &fees).is_err());
 }
