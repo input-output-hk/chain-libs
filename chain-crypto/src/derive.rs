4fa1ee8f584ed98930d82a00d27c6bfc0f7a93af
Sebastien <sebastiengllmt@gmail.com>
2019-10-22 02:49:57 +0900

    add creation from bip39 seed

--- chain-crypto/src/derive.rs
+++ chain-crypto/src/derive.rs
@@ -1,3 +1,4 @@
+use ed25519_bip32::{PrivateKeyError, XPrv, XPRV_SIZE};
 use ed25519_bip32::{DerivationScheme, DerivationError};
 use crate::key::{SecretKey, PublicKey};
 use crate::{Ed25519Bip32, Ed25519, Ed25519Extended, ExtendedPriv, Pub};
@@ -19,14 +20,20 @@ pub fn derive_pk_ed25519(
       .map(PublicKey)
 }
 
-pub fn to_raw_sk(
-    key: &SecretKey<Ed25519Bip32>,
-) -> SecretKey<Ed25519Extended> {
+pub fn to_raw_sk(key: &SecretKey<Ed25519Bip32>) -> SecretKey<Ed25519Extended> {
     SecretKey(ExtendedPriv::from_xprv(&key.0))
 }
 
-pub fn to_raw_pk(
-    key: &PublicKey<Ed25519Bip32>,
-) -> PublicKey<Ed25519> {
+pub fn to_raw_pk(key: &PublicKey<Ed25519Bip32>) -> PublicKey<Ed25519> {
     PublicKey(Pub::from_xpub(&key.0))
+}
+
+pub fn from_bip39_seed(seed: &[u8]) -> Result<SecretKey<Ed25519Bip32>, PrivateKeyError> {
+    if seed.len() != XPRV_SIZE {
+      return Err(PrivateKeyError::LengthInvalid(seed.len()));
+    }
+
+    let mut buf = [0u8; XPRV_SIZE];
+    buf[..].clone_from_slice(seed);
+    Ok(SecretKey(XPrv::normalize_bytes(buf)))
 }
