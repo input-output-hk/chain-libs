545cd690c0b1077848fc9d39183ee969ab629646
Vincent Hanquez <vincent.hanquez@iohk.io>
2020-03-26 14:25:29 +0000

    add chain-block

---
+++ chain-block/src/lib.rs
@@ -0,0 +1,6 @@
+use chain_impl_mockchain as ledger;
+
+pub mod leadership { pub use crate::ledger::leadership; }
+pub mod header { pub use crate::ledger::header; }
+pub mod block { pub use crate::ledger::block; }
+pub mod multiverse { pub use crate::ledger::multiverse; }
