374fcedb3f97e3ac4e15191cdb78bc73e8477e47
dkijania <dariusz.kijania@iohk.io>
2020-04-22 22:19:36 +0200

    Remove serial from builder

--- chain-impl-mockchain/src/testing/builders/stake_pool_builder.rs
+++ chain-impl-mockchain/src/testing/builders/stake_pool_builder.rs
@@ -16,7 +16,6 @@ pub struct StakePoolBuilder {
     operators: Vec<PublicKey<Ed25519>>,
     pool_permissions: Option<PoolPermissions>,
     reward_account: bool,
-    serial: u128,
     tax_type: TaxType,
     alias: String,
 }
@@ -24,7 +23,6 @@ pub struct StakePoolBuilder {
 impl StakePoolBuilder {
     pub fn new() -> Self {
         StakePoolBuilder {
-            serial: 1234,
             owners: Vec::new(),
             operators: Vec::new(),
             alias: "".to_owned(),
@@ -43,11 +41,6 @@ impl StakePoolBuilder {
 
     pub fn with_owners(&mut self, owners: Vec<PublicKey<Ed25519>>) -> &mut Self {
         self.owners.extend(owners);
-        self
-    }
-
-    pub fn with_serial(&mut self, serial: u128) -> &mut Self {
-        self.serial = serial;
         self
     }
 
@@ -113,7 +106,7 @@ impl StakePoolBuilder {
         };
 
         let pool_info = PoolRegistration {
-            serial: self.serial,
+            serial: 0,
             owners: self.owners.iter().cloned().collect(),
             operators: self.operators.iter().cloned().collect(),
             start_validity: DurationSeconds::from(0).into(),
