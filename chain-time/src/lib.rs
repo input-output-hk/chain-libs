eef6bb3bfdf7d3eced02dbcd560783bbf1e4eb99
danielsanchezq <daniel.sanchez@iohk.io>
2020-05-07 16:38:08 +0200

    Move arbitrary implementations to test block

--- chain-time/src/lib.rs
+++ chain-time/src/lib.rs
@@ -1,5 +1,5 @@
-#[macro_use]
-extern crate cfg_if;
+// #[macro_use]
+// extern crate cfg_if;
 
 pub mod era;
 pub mod timeframe;
@@ -11,12 +11,12 @@ pub use timeframe::{Slot, SlotDuration, TimeFrame};
 pub use timeline::{TimeOffsetSeconds, Timeline};
 pub use units::DurationSeconds;
 
-cfg_if! {
-   if #[cfg(test)] {
-        extern crate quickcheck_macros;
-
-        pub mod testing;
-    } else if #[cfg(feature = "property-test-api")] {
-        pub mod testing;
-    }
-}
+// cfg_if! {
+//    if #[cfg(test)] {
+//         extern crate quickcheck_macros;
+//
+//         pub mod testing;
+//     } else if #[cfg(feature = "property-test-api")] {
+//         pub mod testing;
+//     }
+// }
--- chain-time/src/testing.rs
+++ chain-time/src/testing.rs
@@ -1,24 +1,25 @@
-use crate::{Epoch, Slot, TimeEra};
-use quickcheck::{Arbitrary, Gen};
-
-impl Arbitrary for TimeEra {
-    fn arbitrary<G: Gen>(g: &mut G) -> Self {
-        TimeEra::new(
-            Arbitrary::arbitrary(g),
-            Arbitrary::arbitrary(g),
-            u32::arbitrary(g) % 127 + 1,
-        )
-    }
-}
-
-impl Arbitrary for Slot {
-    fn arbitrary<G: Gen>(g: &mut G) -> Self {
-        Slot(Arbitrary::arbitrary(g))
-    }
-}
-
-impl Arbitrary for Epoch {
-    fn arbitrary<G: Gen>(g: &mut G) -> Self {
-        Epoch(Arbitrary::arbitrary(g))
-    }
-}
+// #[cfg(any(test))]
+// use crate::{Epoch, Slot, TimeEra};
+// use quickcheck::{Arbitrary, Gen};
+//
+// impl Arbitrary for TimeEra {
+//     fn arbitrary<G: Gen>(g: &mut G) -> Self {
+//         TimeEra::new(
+//             Arbitrary::arbitrary(g),
+//             Arbitrary::arbitrary(g),
+//             u32::arbitrary(g) % 127 + 1,
+//         )
+//     }
+// }
+//
+// impl Arbitrary for Slot {
+//     fn arbitrary<G: Gen>(g: &mut G) -> Self {
+//         Slot(Arbitrary::arbitrary(g))
+//     }
+// }
+//
+// impl Arbitrary for Epoch {
+//     fn arbitrary<G: Gen>(g: &mut G) -> Self {
+//         Epoch(Arbitrary::arbitrary(g))
+//     }
+// }
