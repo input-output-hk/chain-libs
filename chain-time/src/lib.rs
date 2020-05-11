9e9096f83b9d5a77941b49fc29d0f4d80ddd21a3
danielsanchezq <daniel.sanchez@iohk.io>
2020-05-11 12:39:05 +0200

    Removed commented code

--- chain-time/src/lib.rs
+++ chain-time/src/lib.rs
@@ -1,6 +1,3 @@
-// #[macro_use]
-// extern crate cfg_if;
-
 pub mod era;
 pub mod timeframe;
 pub mod timeline;
@@ -10,13 +7,3 @@ pub use era::{Epoch, TimeEra};
 pub use timeframe::{Slot, SlotDuration, TimeFrame};
 pub use timeline::{TimeOffsetSeconds, Timeline};
 pub use units::DurationSeconds;
-
-// cfg_if! {
-//    if #[cfg(test)] {
-//         extern crate quickcheck_macros;
-//
-//         pub mod testing;
-//     } else if #[cfg(feature = "property-test-api")] {
-//         pub mod testing;
-//     }
-// }
