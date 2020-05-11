61733a9e681ab9de2f9b19ee195c21c3b532200a
danielsanchezq <daniel.sanchez@iohk.io>
2020-05-11 11:01:27 +0200

    Added new utils function to generate an R

--- chain-test-utils/src/utils/mod.rs
+++ chain-test-utils/src/utils/mod.rs
@@ -1,2 +1,7 @@
 pub mod seed;
 pub use smoke::R;
+
+#[allow(non_snake_case)] // Allow because R is a type name, the function is snake case anyway
+pub fn new_R_from_random_seed() -> R {
+    R::from_seed(seed::random_seed())
+}
