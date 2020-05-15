f824d6c09978daeb566bf01615a0ccaeedf5d95d
danielsanchezq <daniel.sanchez@iohk.io>
2020-05-15 10:12:29 +0200

    Added tiny doc and example on Blake generator

--- chain-generators/src/crypto/hash.rs
+++ chain-generators/src/crypto/hash.rs
@@ -1,6 +1,21 @@
 use chain_crypto::hash;
 use chain_test_utils::generators::utils::{Generator, R};
 
+/// A Blake2b256 hash generator. It consumes another generator of values that can be transformed
+/// into a `&[u8]`. For example, a constant hash generator:
+/// ```
+/// use chain_test_utils::generators::utils::ConstantGenerator;
+/// use chain_generators::crypto::hash::Blake2b256Generator;
+/// use chain_test_utils::generators::utils::R;
+/// use rand::random;
+///
+/// let n: u128 = random();
+/// let seed = smoke::Seed::from(n);
+/// let mut r = smoke::R::from_seed(seed);
+/// let value = vec![255u8; 1000];
+/// let const_gen = ConstantGenerator::new(value);
+/// let gen = Blake2b256Generator::new(const_gen);
+/// ```
 pub struct Blake2b256Generator<T, Gen>
 where
     T: AsRef<[u8]>,
@@ -34,13 +49,13 @@ where
 #[cfg(test)]
 mod test {
     use super::*;
-    use chain_test_utils::generators::utils::ConstantGenerator;
+    use chain_test_utils::generators::utils::{ConstantGenerator, Seed, R};
     use rand::random;
     #[test]
     fn generates_ed25519_secret_key() {
         let n: u128 = random();
-        let seed = smoke::Seed::from(n);
-        let mut r = smoke::R::from_seed(seed);
+        let seed = Seed::from(n);
+        let mut r = R::from_seed(seed);
         let value = vec![255u8; 1000];
         let const_gen = ConstantGenerator::new(value);
         let gen = Blake2b256Generator::new(const_gen);
--- chain-test-utils/src/generators/utils/mod.rs
+++ chain-test-utils/src/generators/utils/mod.rs
@@ -1,4 +1,4 @@
 pub mod testing_rng;
-pub use smoke::{Generator, R};
+pub use smoke::{Generator, Seed, R};
 pub mod constant_generator;
 pub use constant_generator::ConstantGenerator;
