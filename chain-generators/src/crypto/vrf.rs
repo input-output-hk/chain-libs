f96b056c0efaa5bc9d0eee569eb19225f300c71f
danielsanchezq <daniel.sanchez@iohk.io>
2020-05-23 13:06:00 +0200

    Make VRF secret key generator composable

--- chain-generators/src/crypto/vrf.rs
+++ chain-generators/src/crypto/vrf.rs
@@ -3,25 +3,21 @@ use chain_test_utils::generators::utils::{Generator, R};
 
 /// This type implements a  `vrf::SecretKey` generator.
 /// It is **not guaranteed** that the generated keys will be unique.
-pub struct VRFSecretKeyGenerator();
+pub struct VRFSecretKeyGenerator<Gen: Generator<Item = u128>> {
+    inner_gen: Gen,
+}
 
-impl VRFSecretKeyGenerator {
-    pub fn new() -> Self {
-        Self::default()
+impl<Gen: Generator<Item = u128>> VRFSecretKeyGenerator<Gen> {
+    pub fn new(inner_gen: Gen) -> Self {
+        Self { inner_gen }
     }
 }
 
-impl Default for VRFSecretKeyGenerator {
-    fn default() -> Self {
-        Self()
-    }
-}
-
-impl Generator for VRFSecretKeyGenerator {
+impl<Gen: Generator<Item = u128>> Generator for VRFSecretKeyGenerator<Gen> {
     type Item = vrf::SecretKey;
 
     fn gen(&self, r: &mut R) -> Self::Item {
-        let scalar = vrf::Scalar::from(R::num::<u128>(r));
+        let scalar = vrf::Scalar::from(self.inner_gen.gen(r));
         // It should be safe to unwrap here since we are using the bytes from a valid Scalar
         vrf::SecretKey::from_bytes(*scalar.as_bytes()).unwrap()
     }
@@ -30,16 +26,23 @@ impl Generator for VRFSecretKeyGenerator {
 #[cfg(test)]
 mod test {
     use super::*;
-    use rand::random;
+    use chain_test_utils::generators::unique::Unique;
+    use smoke::generator::num;
 
     #[test]
     fn generates_vrf_secret_key() {
-        let n: u128 = random();
-        let seed = smoke::Seed::from(n);
-        let mut r = smoke::R::from_seed(seed);
-        let gen = VRFSecretKeyGenerator::new();
+        let (_, mut r) = smoke::R::new();
+        let gen = VRFSecretKeyGenerator::new(num());
         for _ in 0..100 {
             gen.gen(&mut r);
         }
+    }
+
+    #[test]
+    fn generates_unique_vrf_secret_key() {
+        let (_, mut r) = smoke::R::new();
+        let vrf_generator = VRFSecretKeyGenerator::new(Unique::new(num()));
+        let vrf_set: Vec<vrf::SecretKey> = (0..100).map(|_| vrf_generator.gen(&mut r)).collect();
+        assert_eq!(vrf_set.len(), 100);
     }
 }
