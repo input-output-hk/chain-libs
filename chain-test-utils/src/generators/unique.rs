6edf98eb89862b93b579531daff8352119041df8
danielsanchezq <daniel.sanchez@iohk.io>
2020-05-23 13:46:49 +0200

    Added Unique small documentation and example

--- chain-test-utils/src/generators/unique.rs
+++ chain-test-utils/src/generators/unique.rs
@@ -2,6 +2,19 @@ use crate::generators::utils::{Generator, R};
 use std::cell::RefCell;
 use std::collections::HashSet;
 
+/// `Unique` filters the generated items (from other generator) so there are no duplicates.
+/// Internally it uses a `HashSet` to store the visited items.
+/// ⚠ For huge quantities of generated items it may lead to huge times on finding unique items
+/// For example, generate 100 unique random `u64` numbers:
+/// ```
+/// use smoke::generator::{num, Generator};
+/// use std::collections::HashSet;
+/// use chain_test_utils::generators::unique::Unique;
+///
+/// let unique_generator: Unique<u64, _> = Unique::new(num());
+/// let (_, mut r) = smoke::R::new();
+/// let set: HashSet<u64> = (0..100).map(|_| unique_generator.gen(&mut r)).collect();
+/// ```
 pub struct Unique<T, Gen: Generator<Item = T>> {
     inner_generator: Gen,
     visited: RefCell<HashSet<T>>,
