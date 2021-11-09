6f5b29c4c3424fde4ab62fd199cba1272ca5e412
Joaquín Rosales <joaquin.rosales@iohk.io>
2021-11-09 13:53:44 -0600

    remove attribute gates for non-existing feature

--- chain-evm/src/precompiles/native.rs
+++ chain-evm/src/precompiles/native.rs
@@ -180,21 +180,11 @@ impl ExitToNear {
         super::make_address(0xe9217bc7, 0x0b7ed1f598ddd3199e80b093fa71124f);
 }
 
-#[cfg(feature = "contract")]
-fn get_nep141_from_erc20(erc20_token: &[u8]) -> AccountId {
-    AccountId::from_utf8(
-        sdk::read_storage(bytes_to_key(KeyPrefix::Erc20Nep141Map, erc20_token).as_slice())
-            .expect(ERR_TARGET_TOKEN_NOT_FOUND),
-    )
-    .unwrap()
-}
-
 impl Precompile for ExitToNear {
     fn required_gas(_input: &[u8]) -> Result<u64, ExitError> {
         Ok(costs::EXIT_TO_NEAR_GAS)
     }
 
-    #[cfg(not(feature = "contract"))]
     fn run(
         input: &[u8],
         target_gas: Option<u64>,
@@ -208,138 +198,6 @@ impl Precompile for ExitToNear {
         }
 
         Ok(PrecompileOutput::default().into())
-    }
-
-    #[cfg(feature = "contract")]
-    fn run(
-        input: &[u8],
-        target_gas: Option<u64>,
-        context: &Context,
-        is_static: bool,
-    ) -> EvmPrecompileResult {
-        if let Some(target_gas) = target_gas {
-            if Self::required_gas(input)? > target_gas {
-                return Err(ExitError::OutOfGas);
-            }
-        }
-
-        // It's not allowed to call exit precompiles in static mode
-        if is_static {
-            return Err(ExitError::Other(Cow::from("ERR_INVALID_IN_STATIC")));
-        }
-
-        // First byte of the input is a flag, selecting the behavior to be triggered:
-        //      0x0 -> Eth transfer
-        //      0x1 -> Erc20 transfer
-        let mut input = input;
-        let flag = input[0];
-        input = &input[1..];
-
-        let (nep141_address, args, exit_event) = match flag {
-            0x0 => {
-                // ETH transfer
-                //
-                // Input slice format:
-                //      recipient_account_id (bytes) - the NEAR recipient account which will receive NEP-141 ETH tokens
-
-                if is_valid_account_id(input) {
-                    let dest_account = String::from_utf8(input.to_vec()).unwrap();
-                    (
-                        String::from_utf8(sdk::current_account_id()).unwrap(),
-                        // There is no way to inject json, given the encoding of both arguments
-                        // as decimal and valid account id respectively.
-                        format!(
-                            r#"{{"receiver_id": "{}", "amount": "{}", "memo": null}}"#,
-                            dest_account,
-                            context.apparent_value.as_u128()
-                        ),
-                        events::ExitToNear {
-                            sender: context.caller,
-                            erc20_address: events::ETH_ADDRESS,
-                            dest: dest_account,
-                            amount: context.apparent_value,
-                        },
-                    )
-                } else {
-                    return Err(ExitError::Other(Cow::from(
-                        "ERR_INVALID_RECEIVER_ACCOUNT_ID",
-                    )));
-                }
-            }
-            0x1 => {
-                // ERC20 transfer
-                //
-                // This precompile branch is expected to be called from the ERC20 burn function\
-                //
-                // Input slice format:
-                //      amount (U256 big-endian bytes) - the amount that was burned
-                //      recipient_account_id (bytes) - the NEAR recipient account which will receive NEP-141 tokens
-
-                if context.apparent_value != U256::from(0) {
-                    return Err(ExitError::Other(Cow::from(
-                        "ERR_ETH_ATTACHED_FOR_ERC20_EXIT",
-                    )));
-                }
-
-                let erc20_address = context.caller;
-                let nep141_address = get_nep141_from_erc20(erc20_address.as_bytes());
-
-                let amount = U256::from_big_endian(&input[..32]);
-                input = &input[32..];
-
-                if is_valid_account_id(input) {
-                    let receiver_account_id: AccountId = String::from_utf8(input.to_vec()).unwrap();
-                    (
-                        nep141_address,
-                        // There is no way to inject json, given the encoding of both arguments
-                        // as decimal and valid account id respectively.
-                        format!(
-                            r#"{{"receiver_id": "{}", "amount": "{}", "memo": null}}"#,
-                            receiver_account_id,
-                            amount.as_u128()
-                        ),
-                        events::ExitToNear {
-                            sender: erc20_address,
-                            erc20_address,
-                            dest: receiver_account_id,
-                            amount,
-                        },
-                    )
-                } else {
-                    return Err(ExitError::Other(Cow::from(
-                        "ERR_INVALID_RECEIVER_ACCOUNT_ID",
-                    )));
-                }
-            }
-            _ => return Err(ExitError::Other(Cow::from("ERR_INVALID_FLAG"))),
-        };
-
-        let promise: Vec<u8> = PromiseCreateArgs {
-            target_account_id: nep141_address,
-            method: "ft_transfer".to_string(),
-            args: args.as_bytes().to_vec(),
-            attached_balance: 1,
-            attached_gas: costs::FT_TRANSFER_GAS,
-        }
-        .try_to_vec()
-        .unwrap();
-        let promise_log = Log {
-            address: Self::ADDRESS,
-            topics: Vec::new(),
-            data: promise,
-        };
-        let exit_event_log = exit_event.encode();
-        let exit_event_log = Log {
-            address: Self::ADDRESS,
-            topics: exit_event_log.topics,
-            data: exit_event_log.data,
-        };
-
-        Ok(PrecompileOutput {
-            logs: vec![promise_log, exit_event_log],
-            ..Default::default()
-        }
-        .into())
     }
 }
 
@@ -359,7 +217,6 @@ impl Precompile for ExitToEthereum {
         Ok(costs::EXIT_TO_ETHEREUM_GAS)
     }
 
-    #[cfg(not(feature = "contract"))]
     fn run(
         input: &[u8],
         target_gas: Option<u64>,
@@ -373,143 +230,6 @@ impl Precompile for ExitToEthereum {
         }
 
         Ok(PrecompileOutput::default().into())
-    }
-
-    #[cfg(feature = "contract")]
-    fn run(
-        input: &[u8],
-        target_gas: Option<u64>,
-        context: &Context,
-        is_static: bool,
-    ) -> EvmPrecompileResult {
-        if let Some(target_gas) = target_gas {
-            if Self::required_gas(input)? > target_gas {
-                return Err(ExitError::OutOfGas);
-            }
-        }
-
-        // It's not allowed to call exit precompiles in static mode
-        if is_static {
-            return Err(ExitError::Other(Cow::from("ERR_INVALID_IN_STATIC")));
-        }
-
-        // First byte of the input is a flag, selecting the behavior to be triggered:
-        //      0x0 -> Eth transfer
-        //      0x1 -> Erc20 transfer
-        let mut input = input;
-        let flag = input[0];
-        input = &input[1..];
-
-        let (nep141_address, serialized_args, exit_event) = match flag {
-            0x0 => {
-                // ETH transfer
-                //
-                // Input slice format:
-                //      eth_recipient (20 bytes) - the address of recipient which will receive ETH on Ethereum
-                let recipient_address = input
-                    .try_into()
-                    .map_err(|_| ExitError::Other(Cow::from("ERR_INVALID_RECIPIENT_ADDRESS")))?;
-                (
-                    String::from_utf8(sdk::current_account_id()).unwrap(),
-                    // There is no way to inject json, given the encoding of both arguments
-                    // as decimal and hexadecimal respectively.
-                    WithdrawCallArgs {
-                        recipient_address,
-                        amount: context.apparent_value.as_u128(),
-                    }
-                    .try_to_vec()
-                    .map_err(|_| ExitError::Other(Cow::from("ERR_INVALID_AMOUNT")))?,
-                    events::ExitToEth {
-                        sender: context.caller,
-                        erc20_address: events::ETH_ADDRESS,
-                        dest: H160(recipient_address),
-                        amount: context.apparent_value,
-                    },
-                )
-            }
-            0x1 => {
-                // ERC-20 transfer
-                //
-                // This precompile branch is expected to be called from the ERC20 withdraw function
-                // (or burn function with some flag provided that this is expected to be withdrawn)
-                //
-                // Input slice format:
-                //      amount (U256 big-endian bytes) - the amount that was burned
-                //      eth_recipient (20 bytes) - the address of recipient which will receive ETH on Ethereum
-
-                if context.apparent_value != U256::from(0) {
-                    return Err(ExitError::Other(Cow::from(
-                        "ERR_ETH_ATTACHED_FOR_ERC20_EXIT",
-                    )));
-                }
-
-                let erc20_address = context.caller;
-                let nep141_address = get_nep141_from_erc20(erc20_address.as_bytes());
-
-                let amount = U256::from_big_endian(&input[..32]);
-                input = &input[32..];
-
-                if input.len() == 20 {
-                    // Parse ethereum address in hex
-                    let eth_recipient: String = hex::encode(input.to_vec());
-                    // unwrap cannot fail since we checked the length already
-                    let recipient_address = input.try_into().unwrap();
-
-                    (
-                        nep141_address,
-                        // There is no way to inject json, given the encoding of both arguments
-                        // as decimal and hexadecimal respectively.
-                        format!(
-                            r#"{{"amount": "{}", "recipient": "{}"}}"#,
-                            amount.as_u128(),
-                            eth_recipient
-                        )
-                        .as_bytes()
-                        .to_vec(),
-                        events::ExitToEth {
-                            sender: erc20_address,
-                            erc20_address,
-                            dest: H160(recipient_address),
-                            amount,
-                        },
-                    )
-                } else {
-                    return Err(ExitError::Other(Cow::from("ERR_INVALID_RECIPIENT_ADDRESS")));
-                }
-            }
-            _ => {
-                return Err(ExitError::Other(Cow::from(
-                    "ERR_INVALID_RECEIVER_ACCOUNT_ID",
-                )));
-            }
-        };
-
-        let promise = PromiseCreateArgs {
-            target_account_id: nep141_address,
-            method: "withdraw".to_string(),
-            args: serialized_args,
-            attached_balance: 1,
-            attached_gas: costs::WITHDRAWAL_GAS,
-        }
-        .try_to_vec()
-        .unwrap();
-        let promise_log = Log {
-            address: Self::ADDRESS,
-            topics: Vec::new(),
-            data: promise,
-        };
-        let exit_event_log = exit_event.encode();
-        let exit_event_log = Log {
-            address: Self::ADDRESS,
-            topics: exit_event_log.topics,
-            data: exit_event_log.data,
-        };
-
-        Ok(PrecompileOutput {
-            logs: vec![promise_log, exit_event_log],
-            ..Default::default()
-        }
-        .into())
     }
 }
 
