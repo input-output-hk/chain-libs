6bf35b2f030e4c70b8a11bf9a659e4baece7cc66
Mr-Leshiy <leshiy12345678@gmail.com>
2022-01-19 15:10:40 +0200

    add setup env paramets from the test case

--- chain-impl-mockchain/src/ledger/tests/evm_tests.rs
+++ chain-impl-mockchain/src/ledger/tests/evm_tests.rs
@@ -2,6 +2,7 @@ use serde::Deserialize;
 use std::collections::BTreeMap;
 use std::fs::File;
 use std::io::BufReader;
+use std::str::FromStr;
 
 use chain_evm::primitive_types::{H160, H256, U256};
 use chain_evm::state::Account;
@@ -87,6 +88,33 @@ impl EvmStateBuilder {
     }
 }
 
+impl EvmStateBuilder {
+    fn apply_test_evn(mut self, env: TestEnv) -> Self {
+        self = self.set_block_base_fee_per_gas(
+            U256::from_str(&env.current_base_fee).expect("Can not parse base fee"),
+        );
+        self = self.set_block_coinbase(
+            H160::from_str(&env.current_coinbase).expect("Can not parse coinbase"),
+        );
+        self = self.set_block_difficulty(
+            U256::from_str(&env.current_difficulty).expect("Can not parse difficulty"),
+        );
+        self = self.set_block_gas_limit(
+            U256::from_str(&env.current_gas_limit).expect("Can not parse gas limit"),
+        );
+        self = self.set_block_number(
+            U256::from_str(&env.current_number).expect("Can not parse block number"),
+        );
+        self = self.set_block_timestamp(
+            U256::from_str(&env.current_timestamp).expect("Can not parse timestamp"),
+        );
+        self = self.set_block_hashes(vec![
+            H256::from_str(&env.previous_hash).expect("Can not parse previous hash")
+        ]);
+        self
+    }
+}
+
 #[derive(Deserialize)]
 struct TestAccountState {
     balance: String,
@@ -96,41 +124,54 @@ struct TestAccountState {
 }
 
 #[derive(Deserialize)]
-struct TestTransaction {
+#[serde(rename_all = "camelCase")]
+struct TestEvmTransaction {
     data: Vec<String>,
-    gasLimit: Vec<String>,
+    gas_limit: Vec<String>,
     gasPrice: String,
     nonce: String,
-    secretKey: String,
+    secret_key: String,
     sender: String,
     to: String,
     value: Vec<String>,
 }
 
 #[derive(Deserialize)]
+#[serde(rename_all = "camelCase")]
 struct TestEnv {
-    currentBaseFee: String,
-    currentCoinbase: String,
-    currentDifficulty: String,
-    currentGasLimit: String,
-    currentNumber: String,
-    currentTimestamp: String,
-    previousHash: String,
+    current_base_fee: String,
+    current_coinbase: String,
+    current_difficulty: String,
+    current_gas_limit: String,
+    current_number: String,
+    current_timestamp: String,
+    previous_hash: String,
 }
 
 #[derive(Deserialize)]
 struct TestCase {
     pre: BTreeMap<String, TestAccountState>,
-    transaction: TestTransaction,
+    env: TestEnv,
+    transaction: TestEvmTransaction,
 }
 
-#[test]
-fn basic_test() {
-    let file = File::open("../evm-tests/GeneralStateTests/VMTests/vmArithmeticTest/add.json")
-        .expect("Open file failed");
-
+fn run_test(path: &str) {
+    let file = File::open(path).expect("Open file failed");
     let reader = BufReader::new(file);
 
     let test: BTreeMap<String, TestCase> =
         serde_json::from_reader(reader).expect("Parse test cases failed");
+
+    let mut evm_state_builder = EvmStateBuilder::new();
+
+    for (test_name, test_case) in test {
+        dbg!(test_name);
+
+        evm_state_builder = evm_state_builder.apply_test_evn(test_case.env);
+    }
+}
+
+#[test]
+fn vm_add_test() {
+    run_test("../evm-tests/GeneralStateTests/VMTests/vmArithmeticTest/add.json");
 }
