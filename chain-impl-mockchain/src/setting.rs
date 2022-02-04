c6fd95adc691e81d953b1bb3f34de4434ead438d
Joaquín Rosales <joaquin.rosales@iohk.io>
2022-02-04 15:45:59 -0600

    update EVM environment via ledger settings

--- chain-impl-mockchain/src/setting.rs
+++ chain-impl-mockchain/src/setting.rs
@@ -2,7 +2,7 @@
 //!
 
 #[cfg(feature = "evm")]
-use crate::config::EvmConfig;
+use crate::config::{EvmConfig, EvmEnvironment};
 use crate::fragment::{config::ConfigParams, BlockContentSize};
 use crate::milli::Milli;
 use crate::update;
@@ -15,8 +15,6 @@ use crate::{
     rewards,
     vote::CommitteeId,
 };
-#[cfg(feature = "evm")]
-use chain_evm::machine::Environment as EvmEnvironment;
 use std::error::Error;
 use std::fmt;
 use std::num::NonZeroU32;
@@ -256,7 +254,7 @@ impl Settings {
                 }
                 #[cfg(feature = "evm")]
                 ConfigParam::EvmEnvironment(evm_env_params) => {
-                    new_state.evm_environment = evm_env_params.0.clone();
+                    new_state.evm_environment = *evm_env_params.clone();
                 }
             }
         }
@@ -304,6 +302,10 @@ impl Settings {
         };
         #[cfg(feature = "evm")]
         params.push(ConfigParam::EvmConfiguration(self.evm_config));
+        #[cfg(feature = "evm")]
+        params.push(ConfigParam::EvmEnvironment(Box::new(EvmEnvironment(
+            self.evm_environment.0.clone(),
+        ))));
 
         debug_assert_eq!(self, &Settings::new().try_apply(&params).unwrap());
 
