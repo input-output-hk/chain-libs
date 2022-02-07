d0d37b1811db35c7fab4e38cc4001dec9a76c495
Alex Pozhylenkov <leshiy12345678@gmail.com>
2022-02-07 15:12:28 +0200

    Update chain-impl-mockchain/src/certificate/test.rs
    Co-authored-by: Yevhenii Babichenko <eugene.babichenko@iohk.io>

--- chain-impl-mockchain/src/certificate/test.rs
+++ chain-impl-mockchain/src/certificate/test.rs
@@ -352,6 +352,6 @@ fn pool_reg_serialization_bijection(b: PoolRegistration) -> TestResult {
     let result = PoolRegistration::read(&mut buf);
     let left = Ok(b);
     assert_eq!(left, result);
-    assert_eq!(buf.get_slice_end(), &[] as &[u8]);
+    assert!(buf.get_slice_end().is_empty());
     TestResult::from_bool(left == result)
 }
