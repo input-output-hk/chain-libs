2f2a6702d04af3e31603ff1a12b272c62b103eaa
Mr-Leshiy <leshiy12345678@gmail.com>
2022-02-08 10:58:57 +0200

    update suggestions

--- chain-impl-mockchain/src/ledger/tests/mod.rs
+++ chain-impl-mockchain/src/ledger/tests/mod.rs
@@ -4,9 +4,7 @@ pub mod apply_block_tests;
 pub mod certificate_tests;
 pub mod discrimination_tests;
 #[cfg(feature = "evm")]
-pub mod evm_test_suite;
-#[cfg(feature = "evm")]
-mod evm_tests;
+pub mod evm_tests;
 pub mod initial_funds_tests;
 pub mod ledger_tests;
 pub mod transaction_tests;
