9ab452cf321921db81f8b08a91ce675d9c084620
Mr-Leshiy <leshiy12345678@gmail.com>
2022-04-28 15:41:07 +0300

    add is_empty() method

--- chain-impl-mockchain/src/fragment/content.rs
+++ chain-impl-mockchain/src/fragment/content.rs
@@ -58,6 +58,10 @@ impl Contents {
     pub fn len(&self) -> usize {
         self.0.len()
     }
+
+    pub fn is_empty(&self) -> bool {
+        self.0.is_empty()
+    }
 }
 
 #[derive(Clone, Default)]
