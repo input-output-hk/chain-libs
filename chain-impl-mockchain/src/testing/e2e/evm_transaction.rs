c41ad6e3026928f8b904204553c952ac68af1c92
nicopado <nico.pado@hotmail.it>
2022-06-29 12:20:42 +0200

    clean up

--- chain-impl-mockchain/src/testing/e2e/evm_transaction.rs
+++ chain-impl-mockchain/src/testing/e2e/evm_transaction.rs
@@ -8,13 +8,11 @@ const ALICE: &str = "Alice";
 const BOB: &str = "Bob";
 const INITIAL_FUNDS: u64 = 1000;
 const TRANSACTION_AMOUNT: u64 = 100;
-const MAX_GAS_FEE: u64 = 0;
+const MAX_GAS_FEE: u64 = u64::MAX;
 const FIRST_NONCE: u64 = 0;
-//const SECOND_NONCE: u64 = 1;
-//const INVALID_NONCE: u64 = 9845;
 
 #[test] // Simple transaction scenario
-pub fn evm_transaction() {
+pub fn evm_transaction_call_no_data() {
     let (mut ledger, controller) = prepare_scenario()
         .with_initials(vec![
             wallet(ALICE).with(INITIAL_FUNDS).owns("alice_stake_pool"),
@@ -23,10 +21,6 @@ pub fn evm_transaction() {
         .with_config(ConfigBuilder::new().with_evm_params(Config::default()))
         .build()
         .unwrap();
-
-    println!("****************************************************************** START");
-    println!("Ledger Settings: {:?}", ledger.settings());
-    println!("****************************************************************** FLAG 1");
 
     let mut alice = controller.wallet(ALICE).unwrap();
     let mut bob = controller.wallet(BOB).unwrap();
@@ -52,17 +46,16 @@ pub fn evm_transaction() {
 
     let alice_evm_address = ledger.get_evm_mapped_address(&alice.as_account().to_id()).unwrap();
     let bob_evm_address = ledger.get_evm_mapped_address(&bob.as_account().to_id()).unwrap();
-
-    println!("Alice's address:{:?}", alice_evm_address);
-    println!("Bob's address:{:?}", bob_evm_address);
     
     let evm_transaction = TestGen::evm_transaction(alice_evm_address, bob_evm_address, TRANSACTION_AMOUNT, MAX_GAS_FEE, FIRST_NONCE);
-
-    println!("Transaction: {:?}", evm_transaction);
 
     controller.evm_transaction(evm_transaction.clone(), &mut ledger).unwrap();
 
     alice.confirm_transaction();
+
+    LedgerStateVerifier::new(ledger.clone().into())
+        .info("Alice final balance is incorrect.")
+        .account_has_expected_balance(alice.as_account_data(), Value(INITIAL_FUNDS - TRANSACTION_AMOUNT));
     
     LedgerStateVerifier::new(ledger.clone().into())
         .info("Bob final balance is incorrect.")
