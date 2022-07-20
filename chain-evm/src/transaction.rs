6c4cc346c658eeac129d9cd804ad28ccc308e266
Joaquín Rosales <joaquin.rosales@iohk.io>
2022-07-19 23:53:53 -0500

    restore RLP encoding/decoding for unsigned Ethereum transactions

--- chain-evm/src/transaction.rs
+++ chain-evm/src/transaction.rs
@@ -1,8 +1,9 @@
 use crate::{util::Secret, Address};
 use ethereum::{
-    EIP1559TransactionMessage, EIP2930TransactionMessage, LegacyTransactionMessage, TransactionV2,
+    util::enveloped, EIP1559TransactionMessage, EIP2930TransactionMessage,
+    LegacyTransactionMessage, TransactionV2,
 };
-use ethereum_types::H256;
+use ethereum_types::{H256, U256};
 use rlp::{Decodable, DecoderError, Encodable, Rlp, RlpStream};
 use secp256k1::{
     ecdsa::{RecoverableSignature, RecoveryId},
@@ -84,6 +85,98 @@ impl EthereumUnsignedTransaction {
                     },
                 )))
             }
+        }
+    }
+}
+
+impl Decodable for EthereumUnsignedTransaction {
+    fn decode(rlp: &Rlp<'_>) -> Result<Self, DecoderError> {
+        let slice = rlp.data()?;
+
+        let first = *slice.get(0).ok_or(DecoderError::Custom("empty slice"))?;
+
+        let item_count = rlp.item_count()?;
+
+        if item_count == 6 {
+            return Ok(Self::Legacy(LegacyTransactionMessage {
+                nonce: rlp.val_at(0)?,
+                gas_price: rlp.val_at(1)?,
+                gas_limit: rlp.val_at(2)?,
+                action: rlp.val_at(3)?,
+                value: rlp.val_at(4)?,
+                input: rlp.val_at(5)?,
+                chain_id: None,
+            }));
+        }
+        if item_count == 9 {
+            let r = {
+                let mut bytes = [0u8; 32];
+                rlp.val_at::<U256>(7)?.to_big_endian(&mut bytes);
+                H256::from(bytes)
+            };
+            let s = {
+                let mut bytes = [0u8; 32];
+                rlp.val_at::<U256>(8)?.to_big_endian(&mut bytes);
+                H256::from(bytes)
+            };
+            if r == H256::zero() && s == H256::zero() {
+                return Ok(Self::Legacy(LegacyTransactionMessage {
+                    nonce: rlp.val_at(0)?,
+                    gas_price: rlp.val_at(1)?,
+                    gas_limit: rlp.val_at(2)?,
+                    action: rlp.val_at(3)?,
+                    value: rlp.val_at(4)?,
+                    input: rlp.val_at(5)?,
+                    chain_id: rlp.val_at(6)?,
+                }));
+            }
+        }
+
+        let rlp = Rlp::new(slice.get(1..).ok_or(DecoderError::Custom("no tx body"))?);
+
+        if first == 1u8 {
+            if rlp.item_count()? != 9 {
+                return Err(DecoderError::RlpIncorrectListLen);
+            }
+            return Ok(Self::EIP2930(EIP2930TransactionMessage {
+                chain_id: rlp.val_at(1)?,
+                nonce: rlp.val_at(2)?,
+                gas_price: rlp.val_at(3)?,
+                gas_limit: rlp.val_at(4)?,
+                action: rlp.val_at(5)?,
+                value: rlp.val_at(6)?,
+                input: rlp.val_at(7)?,
+                access_list: rlp.list_at(8)?,
+            }));
+        }
+
+        if first == 2u8 {
+            if rlp.item_count()? != 10 {
+                return Err(DecoderError::RlpIncorrectListLen);
+            }
+            return Ok(Self::EIP1559(EIP1559TransactionMessage {
+                chain_id: rlp.val_at(0)?,
+                nonce: rlp.val_at(1)?,
+                max_priority_fee_per_gas: rlp.val_at(2)?,
+                max_fee_per_gas: rlp.val_at(3)?,
+                gas_limit: rlp.val_at(4)?,
+                action: rlp.val_at(5)?,
+                value: rlp.val_at(6)?,
+                input: rlp.val_at(7)?,
+                access_list: rlp.list_at(8)?,
+            }));
+        }
+
+        Err(DecoderError::Custom("invalid tx type"))
+    }
+}
+
+impl Encodable for EthereumUnsignedTransaction {
+    fn rlp_append(&self, s: &mut RlpStream) {
+        match self {
+            Self::Legacy(tx) => tx.rlp_append(s),
+            Self::EIP2930(tx) => enveloped(1, tx, s),
+            Self::EIP1559(tx) => enveloped(2, tx, s),
         }
     }
 }
