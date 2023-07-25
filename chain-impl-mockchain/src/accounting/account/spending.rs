ace69ecb11ab363dd5f196ff9c6f5931c6f3c6ba
Joaquín Rosales <joaquin.rosales@iohk.io>
2023-07-24 18:10:37 -0600

    breaking: remove validation of spending counter for votecast

--- chain-impl-mockchain/src/accounting/account/spending.rs
+++ chain-impl-mockchain/src/accounting/account/spending.rs
@@ -58,9 +58,25 @@ impl SpendingCounterIncreasing {
                 actual: counter,
             })
         } else {
-            self.nexts[counter.lane()] = actual_counter.increment();
+            self.next_unchecked(counter);
             Ok(())
         }
+    }
+
+    /// Increases the spending counter on the given lane.
+    pub(crate) fn next_unchecked(&mut self, unchecked_counter: SpendingCounter) {
+        let lane = unchecked_counter.lane();
+        let counter_to_update = self.nexts[lane];
+        if counter_to_update != unchecked_counter {
+            tracing::warn!(
+                "Invalid spending counter, {}",
+                LedgerError::SpendingCredentialInvalid {
+                    expected: counter_to_update,
+                    actual: unchecked_counter,
+                }
+            );
+        }
+        self.nexts[lane] = counter_to_update.increment();
     }
 }
 
